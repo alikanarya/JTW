22af99b484487633ef8a73d97947a87e067eeeed
Ali Kanarya <akanarya@gmail.com>
2011-01-11 01:36:05 +0200

    1.3.07
    module:imgprocess voidSpace deletion fixed

--- ui_settingsform.h
+++ ui_settingsform.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'settingsform.ui'
 **
-** Created: Mon 10. Jan 17:11:49 2011
+** Created: Mon Jan 10 22:11:33 2011
 **      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
